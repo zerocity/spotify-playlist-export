66c06830aefb932e3158d14fef22abbd373e7f32
jason <jason@duedil.com>
2012-07-31 23:17:47 +0100

    Now outputs a nice dict

--- spe.py
+++ spe.py
@@ -8,18 +8,20 @@ def get_soup(url):
     page = urllib2.urlopen(url)
     return BeautifulSoup(page, convertEntities=BeautifulSoup.HTML_ENTITIES)
 
+
 def parse_spotify_uri(url):
     soup = get_soup(url)
+    return {prop[3:] : soup.find('meta', {'property' : prop})['content'] for prop in target_properties}
 
-    for meta in soup('meta'):
-        #for prop in target_properties:
-        yield [soup.find('meta', {'property' : prop}) for prop in target_properties]
 
 if __name__ == "__main__":
+
     if (len(sys.argv) > 1):
         with open( sys.argv[1], 'r') as fp:
             for line in fp:
+                count = 0
                 for item in parse_spotify_uri(line):
-                    print item
+                    count += 1
+                    print count, item
     else:
         print "USAGE: %s <filename with a list of spotify http uris>" % (sys.argv[0])
